dba149e4d79a847c64c70526cedb1039c7a3d6d5
GenerallyIntelligent <tannerjeffreysims@gmail.com>
2022-02-16 14:16:08 -0700

    create basic repo structure and setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+PACKAGE_ROOT = os.path.dirname(os.path.realpath(__file__))
+README_FILE = open(os.path.join(PACKAGE_ROOT, 'README.md'), 'r').read()
+
+if __name__ == '__main__':
+    setup(
+        name = 'branchml',
+        version = '0.0.0',
+        description = "A framework of models, datasets and other utilities for training branch ML models.",
+        long_description = README_FILE,
+        long_description_content_type = "text/markdown",
+        url = 'https://github.com/with-branch/branchml',
+        author = 'GenerallyIntelligent',
+        author_email = 'tannersims@generallyintelligent.me',
+        license = 'MIT',
+        packages = find_packages(),
+        install_requires = [
+            'torch >=1.10.0, <2.0.0',
+        ]
+    )
