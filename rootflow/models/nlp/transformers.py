f53e368b7ded9c20856595be3a04559abf592e48
GenerallyIntelligent <tannerjeffreysims@gmail.com>
2022-02-28 13:21:44 -0700

    Changes to the NLP models and heads

--- rootflow/models/nlp/transformers.py
+++ rootflow/models/nlp/transformers.py
@@ -1,8 +1,7 @@
 from typing import List, Mapping, Union, Dict
 import torch
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-from transformers.configuration_utils import PretrainedConfig
-from transformers.modeling_outputs import SequenceClassifierOutput
+from torch.nn import Module, ModuleDict, ModuleList
+from transformers import AutoModel, AutoTokenizer
 from rootflow.models.nlp.utils import tokenize_bookends
 
 
@@ -11,13 +10,14 @@ class Tokenizer:
         self, model_name_or_path: str, max_token_length: int, mode: str = "split"
     ) -> None:
         self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+        self.tokenizer = lambda tokenizer_input: self.tokenizer(
+            tokenizer_input, padding="max_length", truncation=False
+        )
         if mode == "split":
-            tokenizer(
-                tokenization_input, padding="max_length", truncation=False
-            )  # Do something with this
             self._tokenize_function = lambda tokenization_input: tokenize_bookends(
                 tokenization_input, max_token_length, tokenizer=self.tokenizer
             )
+        # (Reminders to implement these modes)
         elif mode == "first":
             raise NotImplementedError(f"Mode {mode} is not implemented for Tokenizer!")
         elif mode == "last":
@@ -29,55 +29,47 @@ class Tokenizer:
         return self._tokenize_function(input_strings)
 
 
-class ClassificationTransformer(torch.nn.Module):
-    def __init__(self, model_name_or_path: str, task_shapes: Union[dict, int]) -> None:
-        transformer_model_with_head = (
-            AutoModelForSequenceClassification.from_pretrained(
-                model_name_or_path, num_labels=1
-            )
-        )
-        self.transformer = getattr(
-            transformer_model_with_head, transformer_model_with_head.base_model_prefix
-        )
-        self.config = transformer_model_with_head.config
-        if isinstance(task_shapes, Mapping):
-            self.is_multitask = True
-            self.classification_heads = {
-                task_name: ClassificationHead(self.config, task_shape)
-                for task_name, task_shape in task_shapes.items()
-            }
-        else:
-            self.is_multitask = False
-            self.classification_head = ClassificationHead
+class Transformer(Module):
+    def __init__(
+        self,
+        model_name_or_path: str,
+        head: Union[Module, ModuleList, ModuleDict] = None,
+    ) -> None:
+        self.transformer = AutoModel.from_pretrained(model_name_or_path)
+        self.config = self.transformer.config
+        assert isinstance(
+            head, (Module, ModuleList, ModuleDict, None)
+        ), "Head must be a torch Module or equivalent!"
+        self.head = head
 
+    # Flags instead of type checking would make this faster
     def forward(self, *args, **kwargs):
         transformer_outputs = self.transformer(*args, **kwargs)
-        if self.is_multitask:
+        if isinstance(self.head, ModuleDict):
             return {
                 task_name: task_head(transformer_outputs)
-                for task_name, task_head in self.classification_heads.items()
+                for task_name, task_head in self.head.items()
             }
+        elif isinstance(self.head, ModuleList):
+            return [task_head(transformer_outputs) for task_head in self.head]
+        elif isinstance(self.head, Module):
+            return self.head(transformer_outputs)
         else:
-            return self.classification_head(transformer_outputs)
+            return transformer_outputs
 
 
 # Copied from the huggingface RobertaClassificationHead
-class ClassificationHead(torch.nn.Module):
-    def __init__(self, config, task_shape):
+class ClassificationHead(Module):
+    def __init__(self, input_size, num_classes, dropout: float = 0.1):
         super().__init__()
-        self.dense = torch.nn.Linear(config.hidden_size, config.hidden_size)
-        classifier_dropout = (
-            config.classifier_dropout
-            if config.classifier_dropout is not None
-            else config.hidden_dropout_prob
-        )
-        self.dropout = torch.nn.Dropout(classifier_dropout)
-        self.task_shape = task_shape
-        self.out_proj = torch.nn.Linear(
-            config.hidden_size, task_shape  # Multiply out the shape if necessary
-        )  # Should be able to reshape to whatever task shape was defined
+        self.dense = torch.nn.Linear(input_size, input_size)
+        self.dropout = torch.nn.Dropout(dropout)
+        self.num_classes = num_classes
+        self.out_proj = torch.nn.Linear(input_size, num_classes)
 
     def forward(self, features, **kwargs):
+        # Needs to first load the correct portion of the
+        # model output from the output dictionary
         x = features[0][:, 0, :]
         x = self.dropout(x)
         x = self.dense(x)
