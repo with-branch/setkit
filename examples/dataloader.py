cdcd3c5d786a1b532a45ace7672f3049558df239
hutchdal003 <dallinhutchison@gmail.com>
2022-03-03 21:56:17 -0700

    created two versions of EmailCorpus dataset one with zarr and one with a np.memmap

--- examples/dataloader.py
+++ examples/dataloader.py
@@ -1,13 +1,12 @@
-from rootflow.datasets.examples import ExampleNLP
+from rootflow.datasets.emails.email_corpus import EmailCorpus
 from rootflow.datasets.base import DataLoader
 from rootflow.models.nlp.transformers import Tokenizer
+from tqdm import tqdm
 
-dataset = ExampleNLP()
-tokenizer = Tokenizer("roberta-base", 20)
-dataset.map(tokenizer, batch_size=512)
+dataset = EmailCorpus()
+#tokenizer = Tokenizer("roberta-base", 20)
+#dataset.map(tokenizer, batch_size=512)
 dataloader = DataLoader(dataset, batch_size=128, num_workers=8)
 
-for batch in dataloader:
-    print(batch.keys())
-    print(batch["data"])
-    break
+for batch in tqdm(dataloader):
+    tmep = ""
--- rootflow/datasets/emails/memmap_corpus.py
+++ rootflow/datasets/emails/memmap_corpus.py
@@ -15,8 +15,7 @@ from rootflow.datasets.base import RootflowDataset, RootflowDataItem
 from os.path import exists
 from tqdm import tqdm
 import json
-import numcodecs
-import zarr
+import numpy as np
 from rootflow.datasets.base.utils import (
     map_functions
 )
@@ -24,10 +23,10 @@ from rootflow.datasets.base.utils import (
 class EmailCorpus(RootflowDataset):
     BUCKET = "rootflow"
     DATASET_PATH = "datasets/email-notification"
-    FILE_NAME = "email_data.zarr"
+    FILE_NAME = "email_data.mmap"
     CHUNK_SIZE = 5000
 
-    def __init__(self, prefix: str = "", root: str = None, download: bool = None, tasks = None) -> None:
+    def __init__(self, prefix: str = "", root: str = None, download: bool = True, tasks = None) -> None:
         self.prefix = self.DATASET_PATH + prefix
         super().__init__(root, download, tasks)
 
@@ -44,24 +43,13 @@ class EmailCorpus(RootflowDataset):
     def download(self, directory: str):
         from google.cloud import storage
         file_path = os.path.join(directory, self.FILE_NAME)
-        storage_client = storage.Client()
+        storage_client = storage.Client()        
 
         #count how many files we are going to download
         bucket = storage.Bucket(storage_client, name=self.BUCKET) 
         file_names_iter = storage_client.list_blobs(bucket, prefix = self.prefix)
         num_files = sum(1 for blob in file_names_iter)
-        store = zarr.NestedDirectoryStore(file_path)
-        i = 0
-
-        if  not exists( file_path ):
-            self.data = zarr.create(shape=num_files, chunks=self.CHUNK_SIZE, 
-                store=store, dtype=object, object_codec=numcodecs.MsgPack())
-
-        else:
-            #this is apperently the only way to count how many files there are
-            #sorry for the janky code blame google
-            self.data = zarr.open(file_path, mode='a', shape=num_files, chunks=self.CHUNK_SIZE, dtype=object, object_codec=numcodecs.MsgPack())
-
+        self.data = np.memmap(file_path, dtype=dict, mode='w+', shape=num_files)
         
         print("Downloading files")
         file_names_iter = storage_client.list_blobs(bucket, prefix = self.prefix)
@@ -69,7 +57,7 @@ class EmailCorpus(RootflowDataset):
         #dynamic array could be switched with linked list to avoid resizing
         temp_store = []
         store_index = 0
-        zarr_initial_index = 0
+        mmap_initial_index = 0
         for i, file in tqdm(enumerate(file_names_iter), total=num_files, smoothing=.9):
             data = file.download_as_string()
             json_object = json.loads(data)
@@ -88,21 +76,23 @@ class EmailCorpus(RootflowDataset):
                 store_index += 1
             else:
                 #insert full chunk into zarr
-                self.data[zarr_initial_index:i] = temp_store
-                zarr_initial_index = i
+                self.data[mmap_initial_index:i] = temp_store
+                mmap_initial_index = i
                 temp_store[0] = full_item
                 store_index = 1
 
             if i == num_files - 1:
                 #could have a partial chunk so we insert now
-                self.data[zarr_initial_index:i+1] = temp_store[0:store_index]
+                self.data[mmap_initial_index:i+1] = temp_store[0:store_index]
 
     def prepare_data(self, directory: str) -> List["RootflowDataItem"]:
         file_path = os.path.join(directory, self.FILE_NAME)
         if exists( file_path ):
-            return zarr.open(file_path, mode='a')
+            return np.memmap(file_path, dtype=dict, mode='r+')
         else:
             return FileNotFoundError
+
+dataset = EmailCorpus()
 
 
 
