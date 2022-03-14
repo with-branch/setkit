890fd05a79ae1e879018cfce9868d56007463cb9
hutchdal003 <dallinhutchison@gmail.com>
2022-03-14 13:09:08 -0600

    email corpus is finished

--- examples/dataloader.py
+++ examples/dataloader.py
@@ -9,28 +9,19 @@ from ast import literal_eval
 
 def mbox_map (mbox_string):
     mbox_message = mailbox.mboxMessage(mbox_string)
-    text, _, _ = decode_mbox_email_message(mbox_message)
-    if text == None:
-        text = ""
-    return text
+    # text, _, _ = decode_mbox_email_message(mbox_message)
+    # if text == None:
+    #     text = ""
+    return mbox_message
 
 
-dataset = EmailCorpus(root='/mnt/3913be04-1a62-4a3d-b5c4-b804c51bfe73/branch/datasets/emails/zarr', download=False)
-print(dataset)
+dataset = EmailCorpus(root='/mnt/3913be04-1a62-4a3d-b5c4-b804c51bfe73/branch/datasets/emails/zarr')
 
-for i, data_item in tqdm(enumerate(dataset), total=len(dataset)):
-    mbox_message = mailbox.mboxMessage(data_item["data"])
-    text, _, _ = decode_mbox_email_message(mbox_message)
-    if text == None:
-        text = ""
-    
-    dataset.set(i, text)
-
-print("mapping over the data")
-start = time.time()
-dataset.map(mbox_map)
-end = time.time()
-print(f"The total time to map was {end - start}")
+# print("mapping over the data")
+# start = time.time()
+# dataset.map(mbox_map)
+# end = time.time()
+# print(f"The total time to map was {end - start}")
 
 dataloader = RootflowDataLoader(dataset, batch_size=128, num_workers=8)
 
